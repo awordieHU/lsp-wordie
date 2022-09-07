44094bd06aa42d4a47b6d6e151c3532df8ca9324
akosua <akosua@10.90.98.184>
2022-09-06 23:18:45 -0400

    This is my first github commit.

---
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	/***
+	Name: Akosua Wordie
+
+	author: @awordieHU
+	GithubToken: *in email*
+	  ***/
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+
+	}
+
+}
